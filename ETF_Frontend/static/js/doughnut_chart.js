c5bb6402234edbe33185250da0fb65a74870cac5
dspataru <61765352+dspataru@users.noreply.github.com>
2023-10-03 19:13:33 -0400

    fixing error in doughnut chart

--- ETF_Frontend/static/js/doughnut_chart.js
+++ ETF_Frontend/static/js/doughnut_chart.js
@@ -37,7 +37,6 @@ let highRisk_data = {
 }; 
 
 
-// setting the configurable variables for the donut chart
 let colors = ['#A94064', '#7FFFD4', 'teal', 'pink', '#800080'];
 let bordercolor = '#ffffff';
 let hoverBorderColor = '#000000';
